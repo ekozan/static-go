d977d3a51cd0202806252d1559f7fbfb2aa1ec22
Maxime Franck <geeked7@gmail.Com>
2024-09-09 19:57:09 +0200

    Create server.go

---
+++ src/cmd/server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"text/template"
+)
+
+type StatusRespWr struct {
+	http.ResponseWriter // We embed http.ResponseWriter
+	status              int
+}
+
+func (w *StatusRespWr) WriteHeader(status int) {
+	w.status = status // Store the status for our own use
+	w.ResponseWriter.WriteHeader(status)
+}
+
+func wrapHandler(h http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		srw := &StatusRespWr{ResponseWriter: w}
+
+		h.ServeHTTP(srw, r)
+
+		log.Printf("%s - \"%s %s %s\" %d %d \"%s\"", r.RemoteAddr, r.Method, r.RequestURI,
+			r.Proto, srw.status, r.ContentLength, r.UserAgent())
+	}
+}
+
+func main() {
+	http.HandleFunc("/", wrapHandler(
+		http.FileServer(http.Dir("/static"))))
+
+	// Start server
+	log.Printf("Listen on 8080")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+]
